efecc6e0ce44572bfdf87277ddd975f535767297
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-08-18 13:36:03 -0300

    first commit

---
+++ backend/src/main/java/com/techvista/vistamovie/VistamovieApplication.java
@@ -0,0 +1,13 @@
+package com.techvista.vistamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistamovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VistamovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/techvista/vistamovie/VistamovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techvista.vistamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistamovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
