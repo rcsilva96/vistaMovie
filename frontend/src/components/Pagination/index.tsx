f6e2a9ae17ea0efc059a61227b3ce1f15ded4a91
Renato Carvalho Silva <rsilv1822@gmail.com>
2024-08-22 07:17:01 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import "./styles.css";
 import {ReactComponent as Arrow} from "../../assets/img/arrow.svg";
+import { MoviePage } from "../../types/movie";
 
-function Pagination() {
+type Props = {
+
+  page: MoviePage;
+  onChange: Function;
+}
+
+
+
+function Pagination( { page, onChange }: Props) {
+
   return (
     <div className="vistamovie-pagination-container">
+
       <div className="vistamovie-pagination-box">
-        <button className="vistamovie-pagination-button" disabled={true}>
+
+        <button className="vistamovie-pagination-button" disabled={page.first} onClick={ () => onChange(page.number - 1)}>
           <Arrow />
         </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="vistamovie-pagination-button" disabled={false}>
+
+        <p>{`${page.number + 1 } de ${page.totalPages}`}</p>
+
+        <button className="vistamovie-pagination-button" disabled={page.last} onClick={ () => onChange(page.number + 1)}>
           <Arrow className="vistamovie-flip-horizontal" />
         </button>
+
       </div>
+
     </div>
   );
 }
